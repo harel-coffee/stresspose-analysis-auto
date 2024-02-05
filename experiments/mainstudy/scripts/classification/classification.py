39dc428e65c9c0c9af16bc01dab8abe0895cc4aa
Robert Richer <robert.richer@fau.de>
2024-02-05 10:34:43 +0100

    added mainstudy files

---
+++ experiments/mainstudy/scripts/classification/classification.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+import os
+import warnings
+from pathlib import Path
+
+import biopsykit as bp
+from biopsykit.classification.model_selection import SklearnPipelinePermuter
+from biopsykit.classification.utils import prepare_df_sklearn
+from sklearn.exceptions import ConvergenceWarning
+from sklearn.model_selection import GroupKFold
+
+from stresspose_analysis.classification.hyperparameter_search.mainstudy import (
+    get_hyper_para_dict,
+    get_hyper_search_dict,
+    get_model_dict,
+)
+from stresspose_analysis.classification.utils import flatten_wide_format_column_names
+
+env_vars = {key: val for key, val in os.environ.items() if key.startswith("PARAM__")}
+
+random_state = int(env_vars.pop("PARAM__RANDOM_STATE", 0))
+
+classification_type = env_vars.get("PARAM__TYPE", "general")
+scaler = env_vars.get("PARAM__SCALER", None)
+fsel = env_vars.get("PARAM__FSEL", None)
+
+file_name = Path(__file__).with_suffix("").name
+
+input_path = Path("../../")
+output_path = Path("../../output/classification")
+output_path.mkdir(exist_ok=True)
+
+feature_path = input_path.joinpath("feature_export/movement_features")
+if classification_type == "general":
+    feature_file = feature_path.joinpath("movement_features_for_classification.csv")
+else:
+    feature_file = feature_path.joinpath(f"movement_features_{classification_type}_for_classification.csv")
+
+print(f"Using feature file: {feature_file}")
+
+data = bp.io.load_long_format_csv(feature_file)
+
+levels_unstack = list(data.index.names)
+for level in ["subject", "condition"]:
+    levels_unstack.remove(level)
+data_wide = data["data"].unstack(levels_unstack)
+data_wide = flatten_wide_format_column_names(data_wide)
+
+
+X, y, groups, group_keys = prepare_df_sklearn(data_wide, label_col="condition", print_summary=True)
+num_subjects = len(group_keys)
+
+model_dict = get_model_dict(scaler=scaler, fsel=fsel)
+params_dict = get_hyper_para_dict(num_subjects=num_subjects)
+hyper_search_dict = get_hyper_search_dict()
+
+
+outer_cv = GroupKFold(5)
+inner_cv = GroupKFold(5)
+
+
+base_file_name = f"{file_name}"
+for key, value in env_vars.items():
+    if value is None:
+        continue
+    key_name = key.split("__")[-1].lower()
+    base_file_name += f"_{key_name}_{value}"
+
+base_file_name += "_pipeline_permuter"
+
+# check if pipeline permuter already exists from previous job
+input_file_path = output_path.joinpath(f"{base_file_name}.pkl")
+if input_file_path.exists():
+    print(f"Loading pre-fitted pipeline permuter from {input_file_path}.")
+    pipeline_permuter = SklearnPipelinePermuter.from_pickle(input_file_path)
+else:
+    pipeline_permuter = SklearnPipelinePermuter(model_dict, params_dict, hyper_search_dict, random_state=random_state)
+
+print(f"RANDOM STATE: {pipeline_permuter.random_state.get_state()[1][0]}")
+
+
+data.to_csv(output_path.joinpath(f"{feature_file.stem}.csv"))
+output_file_path = output_path.joinpath(f"{base_file_name}.pkl")
+
+# fit all pipelines
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category=ConvergenceWarning)
+    pipeline_permuter.fit_and_save_intermediate(
+        X,
+        y,
+        file_path=output_file_path,
+        outer_cv=outer_cv,
+        inner_cv=inner_cv,
+        groups=groups,
+        use_cache=False,
+    )
+
+metric_summary = pipeline_permuter.metric_summary()
+metric_summary = metric_summary.sort_values(by="mean_test_accuracy", ascending=False)
+
+print("METRIC SUMMARY")
+print(metric_summary[["mean_test_accuracy", "std_test_accuracy"]].head())
+
+pipeline_permuter.to_pickle(output_file_path)
---
+++ experiments/mainstudy/scripts/feature_extraction/feature_extraction_per_phase.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+
+import json
+from pathlib import Path
+from typing import Tuple
+
+import pandas as pd
+from joblib import Parallel, delayed
+
+from tqdm.auto import tqdm
+
+from empkins_io.datasets.d03.macro_ap01 import MacroStudyTsstDatasetPerPhase
+from empkins_macro.feature_extraction import extract_expert_features, extract_generic_features
+from stresspose_analysis.feature_extraction.utils import (
+    load_generic_feature_dict,
+    load_expert_feature_dict,
+    remove_na,
+)
+
+deploy_type = "hpc"
+system = "xsens"
+
+root_path = Path("../..")
+
+config_dict = json.load(root_path.joinpath("config.json").open(encoding="utf-8"))
+
+feature_dict_path = root_path.joinpath("params/feature_dicts_tsst")
+output_path = root_path.joinpath("feature_export/movement_features")
+output_path.mkdir(exist_ok=True)
+
+base_path = Path(config_dict[deploy_type]["base_path"])
+dataset = MacroStudyTsstDatasetPerPhase(base_path)
+
+sampling_rate = 60  # Hz
+
+threshold_gyr = 5  # deg2/s2
+window_sec_gyr = 0.5  # sec
+overlap_percent_gyr = 0.5  # %
+
+threshold_vel = 5e-5  # m2/s2
+window_sec_vel = 0.5  # sec
+overlap_percent_vel = 0.5  # %
+
+distance_thres = 0.2  # m
+
+generic_feature_dict = load_generic_feature_dict(feature_dict_path)
+expert_feature_dict = load_expert_feature_dict(
+    feature_dict_path,
+    sampling_rate_hz=sampling_rate,
+    threshold_gyr=threshold_gyr,
+    window_sec_gyr=window_sec_gyr,
+    overlap_percent_gyr=overlap_percent_gyr,
+    threshold_vel=threshold_vel,
+    window_sec_vel=window_sec_vel,
+    overlap_percent_vel=overlap_percent_vel,
+    distance_thres=distance_thres,
+)
+
+
+def process_subset(
+    subset: MacroStudyTsstDatasetPerPhase, expert_feature_params, generic_feature_params
+) -> Tuple[Tuple[str, str], pd.DataFrame]:
+    subject_id = subset.index["subject"][0]
+    condition = subset.index["condition"][0]
+
+    phase_dict = {}
+    for subsubset in subset.groupby(["phase"]):
+        phase = subsubset.index["phase"][0]
+        mocap_data = subsubset.mocap_data
+
+        generic_features = extract_generic_features(mocap_data, generic_feature_params, system="xsens")
+        expert_features = extract_expert_features(mocap_data, expert_feature_params, system="xsens")
+        phase_dict[phase] = pd.concat([generic_features, expert_features])
+
+    return_data = pd.concat(phase_dict, names=["phase"])
+    return (subject_id, condition), return_data
+
+
+num_processes = -1
+parallel = Parallel(n_jobs=num_processes, return_as="generator")
+
+group_levels = ["subject", "condition"]
+
+dataset = dataset.get_subset(phase=["talk", "math"]).groupby(group_levels)
+
+results = tqdm(
+    parallel(delayed(process_subset)(subset, expert_feature_dict, generic_feature_dict) for subset in dataset),
+    total=len(dataset),
+)
+
+result_dict = dict(results)
+print("Done extracting features!")
+
+# Data Concatenation
+movement_data_total = pd.concat(result_dict, names=group_levels)
+movement_data_total = remove_na(movement_data_total)
+
+index_levels = movement_data_total.index.names
+movement_data_cleaned = movement_data_total.unstack(group_levels)
+
+# Drop features that are NaN for any subject
+movement_data_cleaned = movement_data_cleaned.dropna(how="any", axis=0)
+# Drop features that are constant (e.g., 0) for all subjects
+std_mask = movement_data_cleaned.std(axis=1) != 0
+movement_data_cleaned = movement_data_cleaned.loc[std_mask]
+
+# Bring dataframe back in original format
+movement_data_cleaned = movement_data_cleaned.stack(group_levels)
+movement_data_cleaned = movement_data_cleaned.reorder_levels(index_levels).sort_index()
+
+# Export Data
+movement_data_total.to_csv(output_path.joinpath("movement_features_per_phase.csv"))
+movement_data_cleaned.to_csv(output_path.joinpath("movement_features_per_phase_cleaned.csv"))
+movement_data_cleaned.to_csv(output_path.joinpath("movement_features_per_phase_for_classification.csv"))
